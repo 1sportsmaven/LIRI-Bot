baece8e3906d1ba0b8b0616c9b62245217057794
Jamal Bailey <bailey0354@gmail.com>
2018-05-18 23:30:28 -0400

    My first attempt at node.js

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'ECnrSZoh9MCezNvRlDhnKHNch',
+    consumer_secret: 'CxcHIC4FAk7QsYSG8p7j8pPKghUSKQuGUtPWg7tI5VFrhho240',
+    access_token_key: '719161875259396097-jIxV043fBvlUfMlsofDI2GUa2rOjVSQ',
+    access_token_secret: 'mELrWRZ91aXs2IcQRkzvFSIsHilpfVhUk8P6m8xnoFhC5',
+  }
+  
+  exports.spotifyKeys = {
+      id: '5cd1d172f98d42cd9296df65e920a800',
+      secret: '13d2cf2f85d04119bdebc354afceef57',
+  }
